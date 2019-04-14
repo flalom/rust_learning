ed96d656fc191c3351651bebef972dc2fff8197d
flalom <esum88@yahoo.com>
2019-04-14 23:04:24 +0200

    From the computerino nice additions!

--- web_scraper/src/main.rs
+++ web_scraper/src/main.rs
@@ -6,16 +6,16 @@ use select::document::Document;
 use select::predicate::Name;
 
 fn main() {
-    hacker_news("https://www.swisstph.ch/en/");
+    link_grabber("https://www.swisstph.ch/");
 }
 
-fn hacker_news(url: &str) {
-    let mut resp = reqwest::get(url).unwrap();
-    assert!(resp.status().is_success());
+fn link_grabber(url: &str) {
+    let resp = reqwest::get(url).unwrap();
+    //assert!(resp.status().is_success());
 
     Document::from_read(resp)
         .unwrap()
         .find(Name("a"))
         .filter_map(|n| n.attr("href"))
-        .for_each(|x| println!("{}", x));
+        .for_each(|x| println!("{}{}", &url, x));
 }
